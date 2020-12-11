699a3c7a263a2be75edada55bd0454c57330f83b
abdullah jamshed <abdullahjamshad01@gmail.com>
2020-12-12 00:21:37 +0500

    map location pin point

--- src/screens/Map.js
+++ src/screens/Map.js
@@ -1,9 +1,11 @@
-import React, {useState} from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {View, Text, StyleSheet, PermissionsAndroid} from 'react-native';
 import MapView, {PROVIDER_GOOGLE, Marker} from 'react-native-maps';
+import Geolocation from 'react-native-geolocation-service';
 
 const Map = () => {
-  const [mar, setMar] = useState({latitude: 24.8825467, longitude: 67.0681097});
+  const [location, setLocation] = useState(null);
+  const [isGranted, setIsGranted] = useState(null);
   const [region, setRegion] = useState({
     latitude: 24.8825467,
     longitude: 67.0681097,
@@ -11,15 +13,49 @@ const Map = () => {
     longitudeDelta: 0.0121,
   });
 
-  const onRegionChange = (region) => {
-    setRegion(region);
+  // const [marker, setMarker] = useState({
+  //   latitude: 24.8825467,
+  //   longitude: 67.0681097,
+  // });
+
+  // const onRegionChange = (region) => {
+  //   setRegion(region);
+  // };
+
+  useEffect(() => {
+    console.log('location ===>>>', location);
+  }, [location]);
+
+  const a = async () => {
+    const granted = await PermissionsAndroid.check(
+      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+    );
+
+    if (granted) {
+      Geolocation.getCurrentPosition(
+        (position) => {
+          setLocation(position.coords);
+          console.log(position.coords);
+        },
+        (error) => {
+          // See error code charts below.
+          console.log('error');
+        },
+        {enableHighAccuracy: true, timeout: 15000, maximumAge: 10000},
+      );
+      // console.log('You can use the ACCESS_FINE_LOCATION');
+    } else {
+      console.log('ACCESS_FINE_LOCATION permission denied');
+    }
   };
+
+  useEffect(() => {
+    a();
+  }, []);
 
   return (
     <View style={styles.container}>
-
-
-      <MapView
+      {/* <MapView
         provider={PROVIDER_GOOGLE} // remove if not using Google Maps
         style={styles.map}
         region={{
@@ -33,7 +69,26 @@ const Map = () => {
           title={`Saylani`}
           description={`Saylani discription`}
         />
-      </MapView>
+      </MapView> */}
+
+      {location && (
+        <MapView
+          provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+          style={styles.map}
+          region={{
+            latitude: location.latitude,
+            longitude: location.longitude,
+            latitudeDelta: 0.0421,
+            longitudeDelta: 0.0421,
+          }}>
+          <Marker
+            coordinate={{
+              latitude: location.latitude,
+              longitude: location.longitude,
+            }}
+          />
+        </MapView>
+      )}
 
       {/* <MapView
         style={styles.map}
@@ -41,7 +96,7 @@ const Map = () => {
         onRegionChange={onRegionChange}>
 
         <Marker
-          coordinate={mar}
+          coordinate={marker}
           title={`Saylani`}
           description={`Saylani discription`}
         />
